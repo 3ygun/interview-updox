6962beddea2b2f0c2513f2ce1a35f8948a0de2e7
David Soller <soller.23@osu.edu>
2017-05-30 23:16:22 -0400

    Added Bootstrap 4 and navbar
    - Bootstrap 4 as it simplifies CSS and that is great! However, I know the
      API isn't totally finalized

--- provider-directory/src/components/App.tsx
+++ provider-directory/src/components/App.tsx
@@ -17,9 +17,23 @@ class App extends React.Component<{}, null> {
     this.manager = new ProviderManager();
   }
 
+  renderNavbar() {
+    return (
+      <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <h3>Provider Directory</h3>
+      </nav>
+    );
+  }
+
   render() {
+    const navbar = this.renderNavbar();
+
     return (
       <div>
+        {navbar}
         <ProviderEditor providerManager={this.manager} />
         <ProviderDisplay providerManager={this.manager} />
       </div>
