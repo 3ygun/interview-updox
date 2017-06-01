239ca8fa666f0591655ac8c644057c7346313262
David Soller <soller.23@osu.edu>
2017-06-01 15:57:48 -0400

    Implemented isPrime which passes all tests

--- gen-prime/src/main/java/com/interview/PrimeNumberGenerator.java
+++ gen-prime/src/main/java/com/interview/PrimeNumberGenerator.java
@@ -8,6 +8,26 @@ class PrimeNumberGenerator implements IPrimeNumberGenerator {
         return new ArrayList<Integer>();
     }
     public boolean isPrime(int value) {
+        if (value < 2) {
+            return false;
+        } else if (value == 2) {
+            return true;
+        } else if (value % 2 == 0) {
+            // isEven and not 2
+            return false;
+        } else {
+            return isPrimeOddOnly(value);
+        }
+    }
+
+    private boolean isPrimeOddOnly(int value) {
+        int max = (int)Math.floor(Math.sqrt(value));
+        for (int i = max; i > 1; i -= 2) {
+            // Is there a number between 2 to sqrt(value) that is evenly divisible? 
+            if (value % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
